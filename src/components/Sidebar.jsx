9eb0bd5231a600a0b125f704398833b43f24696c
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2024-12-30 15:47:18 +0530

    Created a simple logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Sidebar.scss'
+import LogoS from '../assets/images/logo-s.png'
+import LogoSubtitle from '../assets/images/logo_sub.png'
+import Sign from '../assets/images/signature.png'
+
+import {Link} from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className='nav-bar'>
 
-        <Link></Link>
+        <Link className = 'logo' t='/'>
+
+        <img src={LogoS} alt="logo" />
+        <img className='sub-logo' src={Sign} alt="logosub" />
+
+        </Link>
 
     </div>
   )
