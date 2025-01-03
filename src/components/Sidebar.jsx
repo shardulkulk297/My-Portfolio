d90b653c3ac766ef99428f0890fc03bb3a627385
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-01-03 19:46:40 +0530

    Created a routing Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,10 @@ import './Sidebar.scss'
 import LogoS from '../assets/images/logo-s.png'
 import LogoSubtitle from '../assets/images/logo_sub.png'
 import Sign from '../assets/images/signature.png'
-
-import {Link} from 'react-router-dom'
+import sign2 from '../assets/images/sign2.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import {Link, NavLink} from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className='nav-bar'>
@@ -13,8 +15,27 @@ const Sidebar = () => {
 
         <img src={LogoS} alt="logo" />
         <img className='sub-logo' src={Sign} alt="logosub" />
+        <img className='sub-logo2' src={sign2} alt="kulkarni" />
 
         </Link>
+
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+
+            <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+           
+           
+          </NavLink>
+
+          <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+          </NavLink>
+
+          
+          <NavLink exact="true" className="contact-link" activeclassname="active" to="/contact">
+            <FontAwesomeIcon  icon={faEnvelope} color='#4d4d4e' />
+          </NavLink>
+        </nav>
 
     </div>
   )
