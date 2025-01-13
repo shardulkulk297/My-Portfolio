09af374710899697b33da60c11aee312e89ae47b
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-01-13 23:51:15 +0530

    Fixed the animation errors and prod errors'

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,132 +5,132 @@ import { faCss3, faGitAlt, faGithub, faGitSquare, faHtml5, faJava, faJsSquare, f
 import { faCode, faDatabase, faNetworkWired } from '@fortawesome/free-solid-svg-icons'
 import mdb from '../assets/images/mdb.svg'
 import rest from '../assets/images/rest.png'
-
+import './About.scss'
 
 
 const Skills = () => {
     return (
         <>
-        <div className='container skills-page'>
-            <div className='text-zone '>
-                <h1>Skills</h1>
-                {/* <h2>Tech-Savvy Coder with a Passion for Innovation</h2> */}
+            <div className='container skills-page'>
+                <div className='text-zone '>
+                    <h1>Skills</h1>
+                    {/* <h2>Tech-Savvy Coder with a Passion for Innovation</h2> */}
 
-                <div className='mainS'>
-                    <div className='skills-content'>
-                        <div className='skill-text'>
-                            <h2>Programming Languages</h2>
-                        </div>
-                        <div className='icons'>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faJsSquare} color='yellow' />
+                    <div className='mainS'>
+                        <div className='skills-content'>
+                            <div className='skill-text'>
+                                <h2>Programming Languages</h2>
+                            </div>
+                            <div className='icons'>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faJsSquare} color='yellow' />
 
-                            </div>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faPython} color='#00FFFF' />
+                                </div>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faPython} color='#00FFFF' />
 
-                            </div>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faJava} color='red' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className='skills-content'>
-                        <div className='skill-text'>
-                            <h2>Web Technologies, Libraries & Frameworks</h2>
-                        </div>
-                        <div className='icons'>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faHtml5} color='Orange' />
-                            </div>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faCss3} color='Blue' />
-
-                            </div>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faReact} color='Blue' />
-                            </div>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faNode} color='Green' />
-                            </div>
-
-                            <div className='icon'>
-                                <img className='Rest' width={60} height={65} src={rest} alt="MongoDB" />
-
+                                </div>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faJava} color='red' />
+                                </div>
                             </div>
                         </div>
-
-                    </div>
-
-                    <div className='skills-content'>
-                        <div className='skill-text'>
-                            <h2>Data & Development Tools</h2>
-                        </div>
-                        <div className='icons'>
-                            <div className='icon'>
-
-                                <img width={60} src={mdb} alt="MongoDB" />
-
+                        <div className='skills-content'>
+                            <div className='skill-text'>
+                                <h2>Web Technologies, Libraries & Frameworks</h2>
                             </div>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faGitAlt} color='Orange' />
+                            <div className='icons'>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faHtml5} color='Orange' />
+                                </div>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faCss3} color='Blue' />
 
-                            </div>
-                            <div className='icon'>
-                                <FontAwesomeIcon icon={faGithub} color='white' />
+                                </div>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faReact} color='Blue' />
+                                </div>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faNode} color='Green' />
+                                </div>
+
+                                <div className='icon'>
+                                    <img className='Rest' width={60} height={65} src={rest} alt="MongoDB" />
+
+                                </div>
                             </div>
 
                         </div>
 
-                    </div>
+                        <div className='skills-content'>
+                            <div className='skill-text'>
+                                <h2>Data & Development Tools</h2>
+                            </div>
+                            <div className='icons'>
+                                <div className='icon'>
 
-                    <div className='skills-content'>
-                        <div className='skill-text'>
-                            <h2>Soft Skills</h2>
+                                    <img width={60} src={mdb} alt="MongoDB" />
+
+                                </div>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faGitAlt} color='Orange' />
+
+                                </div>
+                                <div className='icon'>
+                                    <FontAwesomeIcon icon={faGithub} color='white' />
+                                </div>
+
+                            </div>
+
                         </div>
-                        <div className='icons'>
-                            <p>Leadership</p>
-                            <p>Communication</p>
-                            <p>People Management</p>
-                            <p>Video Editing</p>
-                            <p>Content Creation</p>
+
+                        <div className='skills-content'>
+                            <div className='skill-text'>
+                                <h2>Soft Skills</h2>
+                            </div>
+                            <div className='icons'>
+                                <p>Leadership</p>
+                                <p>Communication</p>
+                                <p>People Management</p>
+                                <p>Video Editing</p>
+                                <p>Content Creation</p>
+
+                            </div>
 
                         </div>
-
                     </div>
                 </div>
             </div>
-        </div>
-        
-         <div className='stage-cube-cont'>
-                                <div className="cubespinner">
-                                    <div className="face1">
-                                        <FontAwesomeIcon icon={faReact} color='Blue' />
-                                    </div>
-                                    <div className="face2">
-                                        <FontAwesomeIcon icon={faNodeJs} color='Green' />
-                                    </div>
-                                    <div className="face3">
-                                        <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
-                                    </div>
-                                    <div className="face4">
-                                        <FontAwesomeIcon icon={faJsSquare} color='Yellow' />
-                                    </div>
-                                    <div className="face5">
-                                        <FontAwesomeIcon icon={faHtml5} color='Orange' />
-                                    </div>
-                                    <div className="face6">
-                                        <FontAwesomeIcon icon={faGitAlt} color='Red' />
-                                    </div>
-                                </div>
-        
-                            </div>
-        
-      
-        
 
-    </>
-        
+            <div className='stage-cube-cont'>
+                <div className="cubespinner">
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faReact} color='Blue' />
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faNodeJs} color='Green' />
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+                    </div>
+                    <div className="face4">
+                        <FontAwesomeIcon icon={faJsSquare} color='Yellow' />
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faHtml5} color='Orange' />
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faGitAlt} color='Red' />
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+        </>
+
 
 
     )
