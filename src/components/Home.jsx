318688415a2965f70bbc07ad663341ae6c1c6381
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2025-01-15 12:02:55 +0530

    Fixed the image sizing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
             </div>
 
             <div className='self'>
-                <img width={150} src={sk} alt="" />
+            <a href="linked.com/shardulkulk297"><img width={150} src="https://i.ibb.co/qWrPPm9/sk.png" alt="sk" border="0"/></a>
             </div>
 
             <Loader type='pacman' />
